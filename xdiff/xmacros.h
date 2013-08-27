01cf45be2678c05be4431d1d9c19f4a3734dd730
Esteban Santana Santana <ESantana@Curse.com>
2013-08-27 15:27:57 -0500

    Version 0.22: A bug in indexing on 64-bit machines was fixed. Released on Oct 28  2006.

--- xdiff/xmacros.h
+++ xdiff/xmacros.h
@@ -24,26 +24,23 @@
 #define XMACROS_H
 
 
-#define GR_PRIME 0x9e370001UL
-
-
 #define XDL_MIN(a, b) ((a) < (b) ? (a): (b))
 #define XDL_MAX(a, b) ((a) > (b) ? (a): (b))
 #define XDL_ABS(v) ((v) >= 0 ? (v): -(v))
 #define XDL_ISDIGIT(c) ((c) >= '0' && (c) <= '9')
-#define XDL_HASHLONG(v, b) (((unsigned long)(v) * GR_PRIME) >> ((CHAR_BIT * sizeof(unsigned long)) - (b)))
+#define XDL_ADDBITS(v, b) ((v) + ((v) >> (b)))
+#define XDL_MASKBITS(b) ((1UL << (b)) - 1)
+#define XDL_HASHLONG(v, b) (XDL_ADDBITS((unsigned long) (v), b) & XDL_MASKBITS(b))
 #define XDL_PTRFREE(p) do { if (p) { xdl_free(p); (p) = NULL; } } while (0)
 #define XDL_RECMATCH(r1, r2) ((r1)->size == (r2)->size && memcmp((r1)->ptr, (r2)->ptr, (r1)->size) == 0)
-#define XDL_LE32_PUT(p, v) \
-do { \
+#define XDL_LE32_PUT(p, v) do { \
 	unsigned char *__p = (unsigned char *) (p); \
 	*__p++ = (unsigned char) (v); \
 	*__p++ = (unsigned char) ((v) >> 8); \
 	*__p++ = (unsigned char) ((v) >> 16); \
 	*__p = (unsigned char) ((v) >> 24); \
 } while (0)
-#define XDL_LE32_GET(p, v) \
-do { \
+#define XDL_LE32_GET(p, v) do { \
 	unsigned char const *__p = (unsigned char const *) (p); \
 	(v) = (unsigned long) __p[0] | ((unsigned long) __p[1]) << 8 | \
 		((unsigned long) __p[2]) << 16 | ((unsigned long) __p[3]) << 24; \
